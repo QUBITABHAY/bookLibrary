f1a76dcb2f617d38175909eae3e114a5ca095358
QUBITABHAY <codingcontestabhay@gmail.com>
2025-11-26 09:24:01 +0530

    feat: add updatebook route

--- backend/src/routes/book.ts
+++ backend/src/routes/book.ts
@@ -8,6 +8,16 @@ export const createBook = async (req: Request, res: Response) => {
     res.send("All feids required");
   }
 
+  const alreadyExists = await prisma.book.find({
+    where: {
+      title: title,
+    },
+  });
+
+  if (alreadyExists) {
+    res.send("Already Exists");
+  }
+
   await prisma.book.create({
     data: {
       title: title,
@@ -19,4 +29,36 @@ export const createBook = async (req: Request, res: Response) => {
   });
 
   res.send("Data created successfully");
+};
+
+export const updateBook = async (req: Request, res: Response) => {
+  const { bookid, title, authorID, genreID, summary, isbn } = req.body;
+
+  if (!title || !authorID || !genreID || !summary || !isbn) {
+    res.send("All feids required");
+  }
+
+  const getBook = await prisma.book.findFirst({
+    where: {
+      title: title,
+    },
+  });
+
+  if (!getBook) {
+    res.send("This book not exists, create it");
+  }
+
+  await prisma.book.update({
+    where: {
+      id: bookid,
+    },
+
+    data: {
+      title: title,
+      authorId: authorID,
+      genreId: genreID,
+      summary: summary,
+      ISBN: isbn,
+    },
+  });
 };
